df1c9a2f34152a6a70bd6472a6f7e220ee3ac9f9
jen-lee27 <jlee689@myseneca.ca>
2023-11-08 20:25:03 -0500

    update

--- midterm/react/my_app/src/states.js
+++ midterm/react/my_app/src/states.js
@@ -4,24 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Content() {
     const names =["Steve Jobs", "Bill Gates", "Elon Mush","Warren Buffet", "Angelina Jolie", "Jeff Bezos", "MaryWilkes", "Hannah Stone", "Isabela Keller", "SteveWozniak" ];
+    
     const [value, setValue] = useState("");
+    const [arr, setArr] = useState(names);
+    
     const onChange = (event) => {
         const searchVal = event.target.value.toLowerCase();
+        setValue(searchVal);
         let newNames = names.filter(name =>
             name.toLowerCase().includes(searchVal)
         );
         console.log(newNames);
-        setValue(newNames);
+        setArr(newNames);
     }
     return (
         <>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Search by name..." aria-label="search" aria-describedby="basic-addon1" onChange={onChange}/>
-                {/* <h4 className="mx-2 my-1">{value}</h4> */}
+                <input type="text" className="form-control" placeholder="Search by name..." aria-label="search" aria-describedby="basic-addon1" onChange={onChange} value={value}/>
             </div>
             <ul class="list-group">
-                {names.map((name, index) => (
-                    <li className="list-group-item" key={index}>{name}</li>
+                {arr.map((name, index) => (
+                    <li className="list-group-item text-center" key={index}>{name}</li>
                 ))}
             </ul>
       </>
